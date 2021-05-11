d1ac95a961ef4f4a16e11d2c9b1d0bd9a3e82357
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-11 11:14:50 +0200

    Exercise

---
+++ Modulo-2/modulo-2-leccion-2.4-exercise-1-función-multiplicación/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Crea una función que reciba como argumentos dos valores y devuelva como valor de retorno la multiplicación de ambos. Haz tres pruebas con distintos números para comprobar que funciona correctamente y muestra el resultado en la consola usando console.log().
+
+function multiplication (a, b){
+    const result = a * b;
+    return result;
+  }
+  const multiplicationResult = multiplication (5, 5);
+  console.log(multiplicationResult)
+
+
+
+
+
+
+
