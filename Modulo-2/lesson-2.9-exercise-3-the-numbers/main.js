222a93351744bc8aa8c689afda5e0ffbcd5af6c2
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-13 19:55:43 +0200

    exercise

---
+++ Modulo-2/lesson-2.9-exercise-3-the-numbers/main.js
@@ -0,0 +1,20 @@
+`use strict`;
+
+//Vamos a crear una función bestLostNomber que nos devuelve algunos números del array de los números de la serie Lost que tenemos arriba. La función, debe seguir estos pasos:
+
+//1. Crear un nuevo array que contiene solo los números pares que hay en lostNumbers. Para conseguirlo vamos a crear un nuevo array vacío y recorrer el array lostNumbers para al encontrar un número par, meterlo en el nuevo array.
+
+//2. Crear un nuevo array que contiene solo los números múltiplos de 3 que hay en lostNumbers, de una forma similar al anterior.
+
+//3. Devolver una concatenación de los 2 arrays anteriores, es decir, que tendrá primero los números pares y luego los múltiplos de 3.
+
+//Para comprobar que los tenemos todos, vamos a ejecutar la función y a loguearlos (con console.log) uno a uno en la consola en orden.
+
+
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function bestLostNumber(){
+    const parNumbers = [];
+
+}
+const threeMultiple = []
