cd8080d1b8424e423e4d42f24c4e1669abbbe4db
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-13 20:33:37 +0200

    Exercise

---
+++ Modulo-2/lesson-2.9-exercise-5-Bonus-create-your-christmas-tree/main.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+//Para que no nos pille el toro esta Navidad, vamos a crear un código que muestre en consola un árbol de navidad con triángulos (▲). Nosotros le diremos la altura y creará un triángulo con un número igual de lineas que la altura que le hemos pasado. Por ejemplo si le pasamos 5, creará este árbol:
+
+//▲
+//▲▲
+//▲▲▲
+//▲▲▲▲
+//▲▲▲▲▲
