afacc0e9b4be630d32b5e06515f71bb7a06195ad
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-14 07:51:20 +0200

    Not finish

---
+++ Modulo-2/Lesson-2.7-Arrays-and-Bucles/exercise-3/main.js
@@ -0,0 +1,4 @@
+`use strict`;
+
+//A partir de una variable acc con valor 0, construir un bucle que se ejecute 10 veces y sume 2 a la variable acc en cada iteración del bucle. Al acabar el bucle, mostraremos en la consola el texto "El resultado es: X", siendo X el valor de la variable acc.
+
