cce7eef6ea1d5e381c96217690f4bccb85c3f8cb
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-15 21:03:14 +0200

    Exercise

---
+++ Modulo-2/Lesson-2.4-Functions/exercise-9-arrow-functions-everywhere/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//Vamos a rehacer alguno de los ejercicios anteriores con funciones flecha. ¡A lo loco!
+
+//function multiplication (a, b){
+  //  const result = a * b;
+  //  return result;
+  //}
+
+//const multiplicationResult = multiplication (8, 5);
+
+const multiplication = (a, b) => {
+  const result = a * b;
+  return result;
+}
+  
+const multiplicationResult = multiplication (8, 5);
+console.log(multiplicationResult);
+
+
+
+
+
+
