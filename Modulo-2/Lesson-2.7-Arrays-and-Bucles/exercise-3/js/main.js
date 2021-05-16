01fade5e66571fedeb2eb62228a045709e466653
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-16 08:11:20 +0200

    Exercise

---
+++ Modulo-2/Lesson-2.7-Arrays-and-Bucles/exercise-3/js/main.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+//A partir de una variable acc con valor 0, construye un bucle que se ejecute 10 veces y sume 2 a la variable acc en cada iteración del bucle. Al acabar el bucle, muestra en la consola el texto "El resultado es: X", siendo X el valor de la variable acc.
+
+
+
+
+  for (let acc = 0; acc < 20; acc = acc + 2) {
+    console.log(`El resultado es: ${acc}`);
+  }
