c1bda583a274e0c32a01615076aff343616c2368
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 07:04:24 +0200

    not finished

---
+++ Modulo-2/Lesson-2.10-Request-to-the-server/exercise-2-chihuahas-everywhere/js/main.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+/*Sigamos jugando un poco con el Dog API:
+a) Vamos a modificar el ejemplo anterior para que en las fotos de nuestra página salgan sólo perros de la raza Chihuahua (consulta la documentación de este API).
+b) Vamos a encapsular toda la lógica para crear una petición en una función. Añadimos un botón a la página con el título 'Enséñame otro Chihuahua' de forma que al pulsarlo se haga otra petición al servidor de una imagen aleatoria y aparezca una nueva imagen de Chihuaua.
+
+*/
+
+function getDogImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+      const img = document.querySelector("img");
+      img.src = data.message;
+      img.alt = "Un chihuahua";
+    });
+}
+const btn = document.querySelector(".js-chihuahua");
+btn.addEventListener("click", getDogImage);
+
+//{"message":"https:\/\/images.dog.ceo\/breeds\/chihuahua\/n02106550_9432.jpg","status":"success"}
