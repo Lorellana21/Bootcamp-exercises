c9b866ceaf6f88de2e6e316cfe09cf32374f7be9
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 10:44:02 +0200

    not finished

--- Modulo-2/Lesson-2.10-Request-to-the-server/exercise-2-chihuahas-everywhere/js/main.js
+++ Modulo-2/Lesson-2.10-Request-to-the-server/exercise-2-chihuahas-everywhere/js/main.js
@@ -7,9 +7,9 @@ b) Vamos a encapsular toda la lógica para crear una petición en una función. 
 */
 
 function getDogImage() {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(data => {
+  fetch("https://dog.ceo/api/breed/chihuahua/image/random")
+    .then((response) => response.json())
+    .then((data) => {
       const img = document.querySelector("img");
       img.src = data.message;
       img.alt = "Un chihuahua";
@@ -17,5 +17,3 @@ function getDogImage() {
 }
 const btn = document.querySelector(".js-chihuahua");
 btn.addEventListener("click", getDogImage);
-
-//{"message":"https:\/\/images.dog.ceo\/breeds\/chihuahua\/n02106550_9432.jpg","status":"success"}
