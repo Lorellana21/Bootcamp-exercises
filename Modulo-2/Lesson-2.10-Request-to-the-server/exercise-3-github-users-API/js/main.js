0fe01fb9182d64a23abf68224f934a6fec35460c
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 20:41:57 +0200

    exercise

--- Modulo-2/Lesson-2.10-Request-to-the-server/exercise-3-github-users-API/js/main.js
+++ Modulo-2/Lesson-2.10-Request-to-the-server/exercise-3-github-users-API/js/main.js
@@ -1,35 +1,34 @@
 `use strict`;
 
-/*Vamos a seguir explorando el API de GitHub explorando la parte del API para acceder a la info sobre organizaciones. La URL de este API es https://api.github.com/orgs/{orgname}, donde {orgname} es el nombre de la organización en GitHub. Por ejemplo, aquí tenéis la URL para obtener información de la organización Adalab https://api.github.com/orgs/Adalab. Si ponéis esta URL en una nueva pestaña del navegador podréis observar qué datos nos devuelve el API.
+/*Vamos a explorar el API de usuarios de GitHub. 
+La URL de este API es https://api.github.com/users/{username}, donde {username} es el nombre del usuario en GitHub. Por ejemplo, aquí tenéis la URL para obtener información del usuario de Isra https://api.github.com/users/gootyfer. 
 
-El objetivo de este ejercicio es mostrar en una web el listado completo de los repositorios de una organización que hay creados en GitHub. Por ejemplo, para Adalab, el resultado final debería ser similar a este:
-
-Para ello vamos a hacer lo siguiente:
-1. Preparar un input  con un botón para que la usuaria introduzca la organización.
-2. Cuando la usuaria pulse el botón buscar acceder a la información de la organización como primera petición al servidor.
-3. Recoger la información de la URL donde consultar la información de los repositorios de la organización en la respuesta del servidor (en la propiedad repos_url) y hacer una nueva petición a esa URL.
-4. En el último then pintar en nuestra web el nombre de todos los repositorios de la organización en una lista (propiedad name de cada objeto repositorio).
-
+Crear una página en la que haya un input de texto y un botón de buscar. 
+El usuario escribirá en el input un nombre de usuario de GitHub. 
+Prepararemos una función que se ejecute cuando se pulse el botón buscar y que contenga una petición al API para obtener información de ese usuario y así mostrarla en nuestra página.
 */
 
-const btnElement = document.querySelector (".js-btn");
+function getInfo() {
+  //Input donde la usuaria pone su nombre de usuario
+  const username = document.querySelector(".js-input");
+  //valor del input
+  const usernameAcepted = username.value;
 
-function getInfo(){
-    const inputElement = document.querySelector (".js-input");
-    const orgName = inputElement.value;
-    fetch ("https://api.github.com/orgs/"+ {orgName})
+  fetch(`https://api.github.com/users/${usernameAcepted}`)
     .then((response) => response.json())
-    .then((repo) => {
-        const repoUrl = repo.repos_url;
-        return fetch ("https://api.github.com/orgs/" + {orgName} + "/repos")
-    })
-    .then(repoResponse => repoResponse.json())
-    .then(repoData => {
-        const list = document.querySelector (".js-list");
-        list.innerHTML = repoData.name;
-    })
-
+    .then((data) => {
+      //li donde aparece su nombre
+      const nameElement = document.querySelector(".js-name");
+      nameElement.innerHTML = data.name;
+      //li donde aparece su numero de repo
+      const repoNumberElement = document.querySelector(".js-repoNumber");
+      repoNumberElement.innerHTML = data.public_repos;
+      //li donde aparece su avatar
+      const avatarElement = document.querySelector("img");
+      avatarElement.src = data.avatar_url;
+    });
 }
 
-
-btnElement.addEventListener ("click", getInfo);
+//Botón de buscar
+const buttonElement = document.querySelector(".js-btn");
+buttonElement.addEventListener("click", getInfo);
