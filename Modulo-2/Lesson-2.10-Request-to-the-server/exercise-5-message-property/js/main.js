56ee649ce84013c91a911e81e9cbac2323562ac3
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 12:56:57 +0200

    not finished

---
+++ Modulo-2/Lesson-2.10-Request-to-the-server/exercise-5-message-property/js/main.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+/*Vamos a entrar en la URL https://api.github.com/orgs/Adalab a través de nuestro navegador para obtener la respuesta del API de GitHub. A continuación vamos a analizar de qué tipo es cada una de las propiedades que nos está devolviendo. Por ejemplo, de qué tipo son login, id, node_id, is_verified...
+Y si entramos en https://dog.ceo/api/breeds/list ¿de qué tipo es la propiedad message?
+
+*/
+
