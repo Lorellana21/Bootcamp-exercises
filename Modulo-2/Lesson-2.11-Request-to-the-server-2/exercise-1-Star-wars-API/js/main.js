b6d01a5fbe3aec92760496c3462e96833def4a78
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 13:46:26 +0200

    Not finished

---
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-1-Star-wars-API/js/main.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+/*Vamos a explorar un API abierto de información sobre el mundo Star Wars. En esta página tenemos la documentación completa del API y un formulario que nos permite hacer peticiones a la URL que indiquemos. Identifica la siguiente información sobre SWAPI:
+- la URL base del API
+- si necesita autenticación
+- método HTTP que deben usar las peticiones a este API
+- URL para acceder a la info del personaje "Luke Skywalker"
+- URL para acceder a la info de la película "A New Hope"
+- a qué otros recursos puedo acceder desde el API además de personajes y pelis
+- URL para acceder al listado de personajes, ¿está paginada?
+- cómo puedo buscar personajes mediante la URL
+- cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee
+
+*/
+
+
