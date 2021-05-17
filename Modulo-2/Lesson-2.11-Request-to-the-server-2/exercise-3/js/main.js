eea6c3be87d2be55aecf0be20f4a4d240d321400
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 13:51:07 +0200

    not finished

---
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-3/js/main.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+/*En la página de SWAPI o en la que habéis creado en el ejercicio 2 inspecciona las peticiones que has hecho al servidor. Al abrir la pestaña Network aparece vacía así que comienza a hacer peticiones con la pestaña abierta. Con la información que obtienes de esta pestaña averigua:
+- dónde está el método de petición
+- el código de la respuesta (recuerda que 200 es OK)
+- en las cabeceras de la petición busca una llamada user-agent, ¿qué puedes decir de su contenido?
+- la respuesta del servidor en JSON
+- al recargar la página aparecen un montón de peticiones en la pestaña Network, ¿sabrías filtrar solo las que son de AJAX? Pista: antes de fetch las peticiones se hacían con el objeto XMLHttpRequest (XHR)
+
+*/
+
+
