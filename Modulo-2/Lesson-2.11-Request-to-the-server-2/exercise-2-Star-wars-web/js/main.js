7fd2072a3f19594d329d6776ed35fc45ee57b8ad
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-17 13:49:17 +0200

    not finished

---
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-2-Star-wars-web/js/main.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+/*Ahora que conocemos mejor el API de Star Wars vamos a hacer una sencilla web usándolo. En la web aparece una caja de texto donde escribimos el nombre de un personaje (o parte del nombre) y un botón, al hacer click, nuestra web muestra debajo un listado con los personajes que coinciden con la búsqueda indicando su nombre y género.
+
+*/
+
+
