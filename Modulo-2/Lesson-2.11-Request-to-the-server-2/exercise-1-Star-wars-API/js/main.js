107e8808616a3ed27a11a9ebdc62fcadef9da308
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 06:33:36 +0200

    Exercise

--- Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-1-Star-wars-API/js/main.js
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-1-Star-wars-API/js/main.js
@@ -1,16 +1,24 @@
 `use strict`;
 
 /*Vamos a explorar un API abierto de información sobre el mundo Star Wars. En esta página tenemos la documentación completa del API y un formulario que nos permite hacer peticiones a la URL que indiquemos. Identifica la siguiente información sobre SWAPI:
-- la URL base del API
-- si necesita autenticación
-- método HTTP que deben usar las peticiones a este API
-- URL para acceder a la info del personaje "Luke Skywalker"
-- URL para acceder a la info de la película "A New Hope"
-- a qué otros recursos puedo acceder desde el API además de personajes y pelis
-- URL para acceder al listado de personajes, ¿está paginada?
-- cómo puedo buscar personajes mediante la URL
-- cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee
+
+1. la URL base del API: https://swapi.dev/api/
+
+2. si necesita autenticación: No
+
+3. método HTTP que deben usar las peticiones a este API: http https://swapi.dev/api/
+
+4. URL para acceder a la info del personaje "Luke Skywalker": https://swapi.dev/api/people/1/
+
+5. URL para acceder a la info de la película "A New Hope": https://swapi.dev/api/films/:id/
+
+6. a qué otros recursos puedo acceder desde el API además de personajes y pelis: planets, species, starships, vehicles
+
+7. URL para acceder al listado de personajes, ¿está paginada?: 
+
+8. cómo puedo buscar personajes mediante la URL: /people/:id/ -- get a specific people resource
+
+9. cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee: Using the wookiee renderer is easy, just append ?format=wookiee to your urls:
+https://swapi.dev/api/planets/1/?format=wookiee
 
 */
-
-
