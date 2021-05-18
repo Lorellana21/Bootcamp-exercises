9916842d1ed52045eb148ec5e540665f94463e10
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 07:23:27 +0200

    not finished

--- Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-4-meeting-Ls/js/main.js
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-4-meeting-Ls/js/main.js
@@ -1,10 +1,19 @@
 `use strict`;
 
-/*Escribir datos en formularios es muy tedioso para los usuarios. ¡Vamos a cachearlos para facilitarles la vida!
-Prepara un input de texto para el nombre y un párrafo vacío
+/*Prepara un input de texto para el nombre y un párrafo vacío
 Cada vez que la usuaria escriba su nombre (keyUp) tenemos que pintar el valor en el párrafo y guardarlo en localStorage.
 Al recargar la página tenemos que consultar localStorage y, si hay algún nombre guardado, rellenar el input y el párrafo.
 
 */
+const inputElement = document.querySelector(".js-input");
+inputIntroduced = inputElement.value;
+const pa = document.querySelector(".js-pa");
 
+function paintHTML() {
+  pa.innerHTML = inputIntroduced;
+}
 
+inputElement.addEventListener("key up", paintHTML);
+
+localStorage.setItem(inputIntroduced);
+const name = localStorage.getItem(inputIntroduced);
