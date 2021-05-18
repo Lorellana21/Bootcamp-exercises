595c3fd53c4d479964ee8f3337ce6cb2df78063b
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 22:08:58 +0200

    not finished

---
+++ Modulo-2/Lesson-2.12-advanced-DOM/exercise-4-hight-paragraphs/js/main.js
@@ -0,0 +1,5 @@
+`use strict`;
+
+/*Vamos a crear un div en HTML que contenga tres párrafos con un texto aleatorio. Con la ayuda de JavaScript vamos a obtener su tamaño usando la propiedad offsetHeight. Posteriormente y usando el atributo HTML style vamos a configurar que su altura será un tercio de la actual.
+
+*/
