da6d45bcaeb10e991ecc902c138f156e8f78ae6f
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 06:51:12 +0200

    not finished

--- Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-2-Star-wars-web/js/main.js
+++ Modulo-2/Lesson-2.11-Request-to-the-server-2/exercise-2-Star-wars-web/js/main.js
@@ -1,7 +1,26 @@
 `use strict`;
 
 /*Ahora que conocemos mejor el API de Star Wars vamos a hacer una sencilla web usándolo. En la web aparece una caja de texto donde escribimos el nombre de un personaje (o parte del nombre) y un botón, al hacer click, nuestra web muestra debajo un listado con los personajes que coinciden con la búsqueda indicando su nombre y género.
+ */
 
-*/
+//caja de texto
 
+//botón
 
+//valor del input
+
+function getList() {
+  const inputElement = document.querySelector(".js-input");
+  const inputIntroduced = inputElement.value;
+
+  fetch(`https://swapi.dev/api/people/${inputIntroduced}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const dataList = document.querySelector(".js-list");
+      dataList.innerHTML = data.name;
+      dataList.innerHTML = data.gender;
+    });
+}
+
+const btnElement = document.querySelector(".js-btn");
+btnElement.addEventListener("click", getList);
