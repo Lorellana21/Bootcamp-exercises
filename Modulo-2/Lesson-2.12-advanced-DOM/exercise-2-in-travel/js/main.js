eed11c35c7cde52b86cb5c936a6acbdb86617994
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 22:06:14 +0200

    not finished

---
+++ Modulo-2/Lesson-2.12-advanced-DOM/exercise-2-in-travel/js/main.js
@@ -0,0 +1,8 @@
+`use strict`;
+
+/*Crea una página que contenga tres imágenes, usando un select de formulario pregúntale a la usuaria qué ciudad quiere visitar.
+Si la usuaria elige Madrid, haz que en cada imagen se muestre una foto de Madrid
+Haz lo mismo para París y Nueva York
+Nota: Usa imágenes que encuentres por Internet.
+
+*/
