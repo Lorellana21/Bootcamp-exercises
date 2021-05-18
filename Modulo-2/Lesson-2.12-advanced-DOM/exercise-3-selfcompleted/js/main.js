164c0bfbef8d1bf1b0825e3f0c8d4dd8f9cd46e0
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-18 22:07:33 +0200

    not finished

---
+++ Modulo-2/Lesson-2.12-advanced-DOM/exercise-3-selfcompleted/js/main.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+/*Vamos a crear nuestro propio autocompletado de formularios. Para ello vamos a crear un formulario con tres campos:
+Nombre
+Apellidos
+Teléfono
+Por otro lado, en JavaScript tendremos un array de 3 objetos con esa estructura. Es decir, un listado 3 objetos, cada uno con nombre apellidos y teléfono. Pediremos a la usuaria que elija uno de esos 3 con un select con el nombre. Al seleccionarlo se rellenará el formulario automáticamente.
+
+*/
