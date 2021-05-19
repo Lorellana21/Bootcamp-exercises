c52dd4839bafbc805effe6934d4873b98e90e54c
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 21:19:27 +0200

    not finished

---
+++ Modulo-2/Lesson-2.13-Arrays-functional-methods/exercise-1-inflate-marks/js/main.js
@@ -0,0 +1,6 @@
+`use strict`;
+
+/*Tenemos las notas del examen metidas en un array: const marks = [5, 4, 6, 7, 9].
+Vamos a modificar las notas de todas para añadirles 1 punto
+Hay que usar map para crear un nuevo array "inflatedMarks" con las notas modificadas. 
+Mostrar en la consola las notas modificadas */
