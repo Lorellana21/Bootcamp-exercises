b41a7e648816e7755984879efd0fb3ff3f2c9328
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 21:25:28 +0200

    not finished

---
+++ Modulo-2/Lesson-2.13-Arrays-functional-methods/exercise-4-only-the-premium/js/main.js
@@ -0,0 +1,3 @@
+`use strict`;
+
+/*Seguimos con nuestra app de moda y vamos a utilizar el listado de usuarios del ejercicio 3. Pero ahora queremos tener un listado de usuarios (en un array premiumUsers) que solo tenga los usuarios premium. ¿Sabremos hacerlo con filter? */
