61e10a30f92d855629d9a2ada51ddfad16866eee
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 07:50:25 +0200

    not finished

--- Modulo-2/Lesson-2.12-advanced-DOM/exercise-3-selfcompleted/js/main.js
+++ Modulo-2/Lesson-2.12-advanced-DOM/exercise-3-selfcompleted/js/main.js
@@ -7,3 +7,33 @@ Teléfono
 Por otro lado, en JavaScript tendremos un array de 3 objetos con esa estructura. Es decir, un listado 3 objetos, cada uno con nombre apellidos y teléfono. Pediremos a la usuaria que elija uno de esos 3 con un select con el nombre. Al seleccionarlo se rellenará el formulario automáticamente.
 
 */
+const form = [
+  {
+    name: "Lorena",
+    surname: "Orellana",
+    phone: "666 555 888",
+  },
+  {
+    name: "Silvia",
+    surname: "España",
+    phone: "666 111 222",
+  },
+  {
+    name: "Dayana",
+    surname: "Romero",
+    phone: "666 555 333",
+  },
+];
+
+console.log(form[0].name);
+
+const input1 = document.querySelector(".js-input1");
+console.log(input1);
+
+const mother = input1.parentElement;
+
+console.log(
+  `La madre de nuestro elemento es un <${mother.nodeName.toLowerCase()}> y tiene la clase .${
+    mother.className
+  }`
+);
