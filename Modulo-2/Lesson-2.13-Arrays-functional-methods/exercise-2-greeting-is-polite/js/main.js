efe7a4452fe4cc3b8f005302fc3bb3e56dc9e7fd
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 21:21:32 +0200

    not finished

---
+++ Modulo-2/Lesson-2.13-Arrays-functional-methods/exercise-2-greeting-is-polite/js/main.js
@@ -0,0 +1,5 @@
+`use strict`;
+
+/*Estamos creando una aplicación web, y lo primero que queremos hacer es saludar al usuario por su nombre
+Tenemos un array con el listado de usuarios de nuestra aplicación const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada']; y queremos conseguir otro array con los saludos, por ejemplo, 'Bienvenida Yolanda'. 
+¿Podríamos usar map para que nos echase una mano? */
