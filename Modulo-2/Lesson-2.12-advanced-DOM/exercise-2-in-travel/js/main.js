155c8b155045fb5eaae918373d4569e7596c4e80
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 07:19:45 +0200

    Not finished

--- Modulo-2/Lesson-2.12-advanced-DOM/exercise-2-in-travel/js/main.js
+++ Modulo-2/Lesson-2.12-advanced-DOM/exercise-2-in-travel/js/main.js
@@ -1,8 +1,23 @@
 `use strict`;
 
-/*Crea una página que contenga tres imágenes, usando un select de formulario pregúntale a la usuaria qué ciudad quiere visitar.
+/*Crea una página que contenga tres imágenes, y usando un select de formulario pregúntale a la usuaria qué ciudad quiere visitar.
 Si la usuaria elige Madrid, haz que en cada imagen se muestre una foto de Madrid
 Haz lo mismo para París y Nueva York
 Nota: Usa imágenes que encuentres por Internet.
 
 */
+
+const nameInput = document.querySelector("#cities");
+
+console.log(nameInput.value); // Pintará el valor actual, 'madrid'
+
+/*
+Para poner un estilo, lo asignaremos usando la siguiente notación
+style.propiedadCSS = 'valor'
+
+De esta forma no modificaremos los estilos previamente asignados
+
+Si la propiedad CSS tiene un guión (background-color) lo convertiremos a camelcase
+backgroundColor
+*/
+nameInput.style.backgroundImage = url("./images/madrid.jpg");
