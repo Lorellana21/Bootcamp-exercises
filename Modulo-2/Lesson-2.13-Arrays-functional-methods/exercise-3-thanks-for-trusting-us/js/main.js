359a5183d6e2b27799b721ff0c4475942287cadc
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-20 11:14:09 +0200

    Exercise

--- Modulo-2/Lesson-2.13-Arrays-functional-methods/exercise-3-thanks-for-trusting-us/js/main.js
+++ Modulo-2/Lesson-2.13-Arrays-functional-methods/exercise-3-thanks-for-trusting-us/js/main.js
@@ -1,13 +1,34 @@
 `use strict`;
 
-/*Seguimos desarrollando nuestra aplicación web que romperá el mercado. Pero antes, queremos agradecer a nuestros usuarios premium su ayuda en el saludo de la aplicación. Por tanto, a los usuarios premium queremos saludarles así 'Bienvenida Yolanda. Gracias por confiar en nosotros.', y mantener el saludo simple 'Bienvenida Yolanda' para el resto de usuarios.
+/*A los usuarios premium queremos saludarles así 'Bienvenida Yolanda. Gracias por confiar en nosotros.', y mantener el saludo simple 'Bienvenida Yolanda' para el resto de usuarios.
 Vamos a partir de este array con el listado de usuarios que incluye tanto su nombre como si son usuarios premium o no.
 Tenemos que crear un nuevo array con los saludos. ¿Podremos hacerlo con map? */
 
 const users = [
-  { name: "María", isPremium: false },
   { name: "Lucía", isPremium: true },
+
+  { name: "María", isPremium: false },
   { name: "Susana", isPremium: true },
   { name: "Rocío", isPremium: false },
   { name: "Inmaculada", isPremium: false },
 ];
+console.log(users);
+const greetings = [
+  "Bienvenida Yolanda. Gracias por confiar en nosotros.",
+  "Bienvenida Yolanda",
+];
+console.log(greetings);
+
+const newGreetings = users.map((name) =>
+  name.isPremium === true
+    ? console.log("Bienvenida Yolanda. Gracias por confiar en nosotros.")
+    : console.log("Hola Yolanda")
+);
+
+/*const sayHi = users.map((premium) => {
+  if (premium.isPremium === false) {
+    console.log("Bienvenida Yolanda");
+  } else {
+    console.log("Bienvenida Yolanda. Gracias por confiar en nosotros.");
+  }
+});*/
