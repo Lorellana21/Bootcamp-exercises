11a67290599cd1b361554838718060cef7c99c13
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-31 20:58:35 +0200

    exercise

--- Módulo-3/ejercicio-3/src/components/App.js
+++ Módulo-3/ejercicio-3/src/components/App.js
@@ -1,29 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 import "../stylesheets/App.css";
 
-class App extends Component {
-  render() {
-    const appParagraph = (
-      <p className="App-intro">
-        Lorem ipsum dolor sit amet consectetur adipiscing elit mauris dui porta
-        fermentum, fames sociosqu blandit dictum arcu ridiculus duis sodales
-        nulla parturient, accumsan consequat donec augue primis vehicula non
-        mollis urna cum. Sociis imperdiet curabitur nulla condimentum urna
-        interdum velit integer metus fermentum, vestibulum rhoncus gravida ante
-        venenatis duis nostra a hac, massa nunc taciti aptent magna nam aliquam
-        dui ut. Ullamcorper rhoncus et sem id ante aenean penatibus parturient
-        cum sapien vitae potenti, odio interdum risus senectus justo litora
-        scelerisque donec mollis habitasse.
-      </p>
-    );
-    const appBody = (
-      <div className="App-body">
-        <h1 className="App-title">Alex Guerrero</h1>
-        <h2>Lunes 26 de junio de 2017</h2>
-      </div>
-    );
-    return [appBody, appParagraph];
-  }
+//Tenemos un objeto data que contiene la siguiente información:
+const data = {
+  image:
+    "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
+  cardTitle: "Bob Dylan",
+  cardDate: "May 24, 1941",
+  cardDescription:
+    "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+  button: {
+    url: "https://en.wikipedia.org/wiki/Bob_Dylan",
+    label: "Go to wikipedia",
+  },
+};
+
+//Usa la información contenida en data para renderizar la tarjeta utilizada anteriormente, por ejemplo: El título de la tarjeta sería el data.cardTitle, etc.
+
+const Header = (
+  <header className="App-header">
+    <img src={data.image} className="App-foto" alt="foto" />
+    <div className="App-header-title">
+      <h1 className="App-header-maintitle">{data.cardTitle}</h1>
+      <h3 className="App-header-subtitle">{data.cardDate}</h3>
+    </div>
+  </header>
+);
+const Main = (
+  <main className="App-main">
+    <p className="App-main-paragraph">{data.cardDescription}</p>
+  </main>
+);
+
+const Footer = (
+  <footer className="App-footer">
+    <p className="App-footer-paragraph">{data.button.label}</p>
+    <div className="App-footer-container">
+      <p className="App-footer-paragraph">37</p>
+    </div>
+  </footer>
+);
+
+const appRoot = (
+  <div className="App">
+    {Header}
+    {Main}
+    {Footer}
+  </div>
+);
+function App() {
+  return appRoot;
 }
 
 export default App;
