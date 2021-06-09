43c1eb2e1b1ab033f51fe0c84e7b848a8be1a106
Lorellana21 <lorellana.ordonez@gmail.com>
2021-06-09 20:49:10 +0200

    exercise

---
+++ Módulo-3/Lesson-3.7-Forms/exercise-2-form-writer/src/components/App.js
@@ -0,0 +1,69 @@
+import "../stylesheets/App.css";
+import React from "react";
+
+class MovieForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      textarea: "",
+      select: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  //aqui manjeo los 3
+  handleChange = (event) => {
+    const name = event.target.name;
+    const textarea = event.target.value;
+    const select = event.target.value;
+    const val = event.target.value;
+    this.setState({
+      [name]: val,
+      [textarea]: val,
+      [select]: val,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+          />
+          <label>
+            Descripción:
+            <textarea
+              id="textarea"
+              name="textarea"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Elige tu idioma:
+            <select name="select" onChange={this.handleChange}>
+              <option selected value="español">
+                Español
+              </option>
+              <option value="inglés">Inglés</option>
+              <option value="portugués">Portugués</option>
+            </select>
+          </label>
+        </form>
+        <article>
+          <p>Tu peli favorita es: {this.state.name}</p>
+          <p>Trata sobre: {this.state.textarea}</p>
+          <p>Idioma: {this.state.select}</p>
+        </article>
+      </>
+    );
+  }
+}
+export default MovieForm;
+
+// Refactorizar el código del ejercicio anterior: usaremos una única función escuchadora para el evento change de cada campo. Para eso, recordad que podemos acceder a la clave de un objeto cuyo nombre tenemos en una variable usando el operador [key] siendo key la variable que tiene el nombre de un campo del objeto.
