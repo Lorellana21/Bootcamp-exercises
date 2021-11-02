c62e1706fd2e8f269b3a4ce869cd54be8e25291b
Lorellana <lorellana.ordonez@gmail.com>
2021-11-02 12:52:54 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.03-Conditionals/exercise-8-Module/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.03-Conditionals/exercise-8-Module/js/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
+//Planteamiento:
+// año = 3;
+// divido 3 entre 4
+// resto = 3;
+// lo que me queda para que sea bisiesto = 4 - 3 = 1;
+// año + lo que me queda = 3 + 1 = 4;
+
+
+
+ //El año es (el que sea)
+// Si lo divido entre 4 el resto es 0?
+// Si es asi ese año es bisiesto
+
 
 const actualYear = 2021;
-//Para obtener el resto de la división:
+//Hallo el "resto" de la división y lo guardo en una constante:
 const module = actualYear % 4;
 //Años que quedan para que llegue el bisiesto:
 const remainingYear = 4 - module;
