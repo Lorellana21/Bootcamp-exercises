bb5eaa80b6fb90ef4db422a4ab5b4331f2f483fc
Lorellana <lorellana.ordonez@gmail.com>
2021-11-02 10:48:21 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.03-Conditionals/exercise-6-Ternary/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.03-Conditionals/exercise-6-Ternary/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-let avocados;
- const avocadoPrice = 1.5;
- const money = 33;
+ // let avocados;
+//  const avocadoPrice = 1.5;
+//  const money = 33;
 
 // if (money >= avocadoPrice) {
 // avocados = money / avocadoPrice;
@@ -12,9 +12,19 @@ let avocados;
 // }
 
 
+//Con el ternario y declarando constante al mismo tiempo que el ternario:
+// const avocadoPrice = 1.5;
+// const money = 33;
+// var avocados = money >= avocadoPrice ? avocados = money / avocadoPrice : avocados = 0;
 
+
+
+//Another way of doing it. Primero declaro constantes y luego el ternario:
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
 
 money >= avocadoPrice ? avocados = money / avocadoPrice : avocados = 0;
 
-console.log(avocados)
+console.log(avocados);
 
