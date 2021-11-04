e46ea16a132505cdda3fc7e942175bccd3341a6a
Lorellana <lorellana.ordonez@gmail.com>
2021-11-04 12:58:28 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-5-querySelector-for-everyone/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-5-querySelector-for-everyone/js/main.js
@@ -1,11 +1,6 @@
 'use strict';
 
-//Estamos trabajando en un proyecto bastante grande, donde hay que recoger muchos elementos de HTML desde JavaScript para interaccionar con ellos. Para no tener que escribir document.querySelector(...) tantas veces una compañera ha sugerido hacer una función llamada getEl.
-
-/
-  
-
-
+ 
 function getEl(sel){
   return document.querySelector(sel);
 }
@@ -13,11 +8,11 @@ function getEl(sel){
 
 const titleElement = getEl('.js-title');
 console.log(titleElement);
-
 titleElement.innerHTML = "Hola";
 
+const textElement = getEl('.js-text');
+console.log(textElement);
+textElement.innerHTML = "soy yo";
 
-
-
-
-
+const listElement = getEl('.js-list');
+console.log(listElement);
