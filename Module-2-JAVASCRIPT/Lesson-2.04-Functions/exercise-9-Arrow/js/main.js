68f3f7da02ffc65392f3b12b189dfa98e62ccb61
Lorellana <lorellana.ordonez@gmail.com>
2021-11-05 11:37:00 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-9-Arrow/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-9-Arrow/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//1
 const getEven = (number) => {
   if(number % 2 === 0){
     console.log("true");
@@ -10,14 +10,23 @@ const getEven = (number) => {
 }
 getEven(20);
 
+//2
 const getIva = (price) => {
   const IVA = price * 0.21;
   const total = price + IVA;
   return total;
 }
-
 const resultIva = getIva(10);
 console.log(resultIva);
+
+
+//3
+const multiplication = (a, b) => {
+  const result = a * b;
+  return result;
+}
+const multiplicationResult = multiplication (8, 5);
+console.log(multiplicationResult);
 
 
 
--- Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-9-arrow-functions-everywhere/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.04-Functions/exercise-9-arrow-functions-everywhere/js/main.js
@@ -9,13 +9,7 @@
 
 //const multiplicationResult = multiplication (8, 5);
 
-const multiplication = (a, b) => {
-  const result = a * b;
-  return result;
-}
-  
-const multiplicationResult = multiplication (8, 5);
-console.log(multiplicationResult);
+
 
 
 
