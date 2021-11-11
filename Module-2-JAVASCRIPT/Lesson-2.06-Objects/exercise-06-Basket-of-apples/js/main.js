7086e1f44af9d71aae6ae0bf9b0f4add7fe2be33
Lorellana <lorellana.ordonez@gmail.com>
2021-11-11 17:27:10 +0100

    done

---
+++ Module-2-JAVASCRIPT/Lesson-2.06-Objects/exercise-06-Basket-of-apples/js/main.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const plus = document.querySelector(".buttonPlus");
+const minus = document.querySelector(".buttonMinus");
+const reset = document.querySelector(".buttonReset");
+const totalApples = document.querySelector(".totalApples");
+
+
+const basket = {};
+basket.max = 10;
+basket.min = 0;
+basket.actual = 0;
+basket.initial = 0;
+
+basket.addApple = function (a) {
+  if (basket.actual < basket.max) {
+    basket.actual += 1;
+  }
+  totalApples.innerHTML = basket.actual;
+};
+
+basket.restApple = function (a) {
+  if (basket.actual > basket.min) {
+    basket.actual -= 1;
+  }
+  totalApples.innerHTML = basket.actual;
+};
+
+basket.resetApple = function (a) {
+  a = basket.initial;
+
+  totalApples.innerHTML = a;
+};
+
+plus.addEventListener("click", basket.addApple);
+minus.addEventListener("click", basket.restApple);
+reset.addEventListener("click", basket.resetApple);
+
+// Other way of doing it:
+
+// const basket = {
+//   maxApples : 24,
+//   minApples : 4,
+//   currentApples : 10,
+//   initApples : 5,
+//   }
+
+// basket.addApple = function (){
+//   return (this.currentApples = this.currentApples + 1);
+// }
+// console.log(basket.addApples());
+
+
+// basket.removeApple = function(){
+//   return (this.currentApples = this.currentApples - 1);
+// }
+// console.log(basket.removeApple());
+
+
+// basket.setApples = function(){
+//   return (this.currentApples = this.initApples);
+// }
+// console.log(basket.setApples());
---
+++ Module-2-JAVASCRIPT/Lesson-2.06-Objects/exercise-06-Basket-of-apples/readme.md
@@ -0,0 +1,35 @@
+## Crear una cesta de manzanas
+
+Vamos a crear un objeto que sea una cesta de manzanas y que debe tener varias propiedades:
+
+- Número máximo de manzanas
+- Número mínimo de manzanas
+- Número actual de manzanas
+- Número inicial de manzanas
+
+Y varios métodos que hagan:
+
+- Añadir al cesto una manzana
+- Sacar del cesto una manzana
+- Restablecer el número de manzanas al valor inicial.
+
+Con la ayuda de `console.log` probaremos a usar varios métodos distintos y comprobar el número actual de manzanas para ver si funciona correctamente.
+
+---
+
+## Create a basket of apples
+
+Create an object which is a basket of apples and which must have several properties:
+
+- Maximum number of apples
+- Minimum number of apples
+- Current number of apples
+- Starting number of apples
+
+And several methods that do:
+
+- Add an apple to the basket
+- Remove an apple from the basket
+- Reset the number of apples to the initial value.
+
+With the help of `console.log` we will try using several different methods and check the current number of apples.
