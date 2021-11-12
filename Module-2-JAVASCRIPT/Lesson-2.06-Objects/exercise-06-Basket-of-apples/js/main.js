efe5ca118a347ceb851e9b26857c2a89d6a50341
Lorellana <lorellana.ordonez@gmail.com>
2021-11-12 11:19:40 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.06-Objects/exercise-06-Basket-of-apples/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.06-Objects/exercise-06-Basket-of-apples/js/main.js
@@ -62,3 +62,36 @@ reset.addEventListener("click", basket.resetApple);
 //   return (this.currentApples = this.initApples);
 // }
 // console.log(basket.setApples());
+
+
+// Other way:
+
+// const basket = {
+//   maxNumer: 10,
+//   minNumber: 5,
+//   currentNumber: 7,
+//   initialNumber: 6,
+//   add: function () {
+//     this.currentNumber++;
+//   },
+//   remove: function () {
+//     this.currentNumber--;
+//   },
+//   reset: function () {
+//     this.currentNumber = this.initialNumber;
+//   }
+// };
+
+// console.log(basket.currentNumber);
+// basket.add();
+// console.log(basket.currentNumber);
+// basket.add();
+// console.log(basket.currentNumber);
+// basket.add();
+// console.log(basket.currentNumber);
+// basket.add();
+// console.log(basket.currentNumber);
+// basket.remove();
+// console.log(basket.currentNumber);
+// basket.reset();
+// console.log(basket.currentNumber);
