66ef14b92ffbc7c7b5a99566c5cbfbf3e0e94ba1
Lorellana <lorellana.ordonez@gmail.com>
2021-11-19 17:39:14 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-04-My-task-list/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-04-My-task-list/js/main.js
@@ -1,9 +1,20 @@
 'use strict';
+/*
+Proceso 1:
+1- Cuando arranque la página pinto el listado de tareas.
+2- Escuchar un evento sobre las tareas pintadas
 
-const textElement = document.querySelector ('.js-text');
-const listElement = document.querySelector ('.js-list');
+Proceso 2:
+1. Una vez que la usuaria ha clicado:
+- recoger en qué elemento ha hecho click
+- modificar datos (estado de la tarea)
+- volver a pintar mis tareas
+- escuchar evento
+*/
 
+const ulElement = document.querySelector ('.js-list');
 
+/*Cada posición de este array es un objeto que tiene 2 propiedades*/
 const tasks = [
   { name: 'Recoger setas en el campo', completed: true },
   { name: 'Comprar pilas', completed: true },
@@ -14,53 +25,48 @@ const tasks = [
   }
 ];
 
-// textElement.innerHTML += `Hay ${tasks.length} tareas`;
-
-
 function paintTasks(){
-  let html = "";
+  let html = ""; //variable que contiene los li
   let className = "";
   let checked = "";
-  for (let i = 0; i < tasks.length; i++){
+
+  for(let i = 0; i < tasks.length; i++){
     let task = tasks[i];
+     
     if(task.completed === true){
       className = "completed-task";
       checked = "checked";
-    } else {
+    }
+    else{
       className = "";
       checked = "";
     }
-    html += `<li class="${className}">`;
-    html += `<input type="checkbox" class="js-checkbox" value="${i}" ${checked}/>`;
-    html += `${task.name}</li>`;
-}
-listElement.innerHTML = html;
-listenClickChecked();
-}
-paintTasks();
-
-function listenClickChecked(){
-  const checkboxElements = document.querySelectorAll (".js-checkbox");
-  for(let i = 0; i < checkboxElements.length; i++){
-    checkboxElements[i].addEventListener("change", handlerCheck);
+    //Se pinta en tres líneas por mejor visualización, pero puede ir perfectamente en una sola.
+    html += `<li class="${className}">`
+    html += `<input class="js-checkbox" type="checkbox" value="${i}" ${checked}/>`
+    html += `${task.name}</li>`
   }
+  ulElement.innerHTML = html;
+  listenClick();
+}
+
+function listenClick(){
+  const checkboxElements = document.querySelectorAll ('.js-checkbox');//Aqui se crea un array
+//Ahora tengo que escuchar un evento sobre cada uno de los elementos del array, por eso hay que recorrerlo.
+for(let i = 0; i < checkboxElements.length; i++){
+  checkboxElements[i].addEventListener("change", handlerCheck);
+}
 }
 
 function handlerCheck(event){
-  const clicked = event.target.value;
-  if(tasks[clicked].completed === true){
-    tasks[clicked].completed = false;
-  } else {
-    tasks[clicked].completed = true;
-  }
-  paintTasks();
+console.log(event.target.value);
+const clicked = event.target.value;
+tasks[clicked].completed = !tasks[clicked].completed;//Le pone el valor contrario a lo que tiene. Solo sirve para booleanos
+//Volver a pintar:
+paintTasks();
 
-
-// // Bucle que recorra el array para extraer los true y false en completed
-//   for(let i = 0; i < clicked.length; i++){
-//     const results = `Tienes ${tasks.length} tareas. ${tasks.completed} completadas y ${tasks.completed} por realizar`;
-//     //Añado párrafo para las Tareas totales
-//     textElement.innerHTML = `<p class="${results}"></p>`;
-//   }
-  
 }
+
+
+
+paintTasks();
