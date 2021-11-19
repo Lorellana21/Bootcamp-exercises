02da8fa91956a7bdb2ba92edc54c4ca04d7bf088
Lorellana <lorellana.ordonez@gmail.com>
2021-11-19 12:06:38 +0100

    nor finished

--- Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-04-My-task-list/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-04-My-task-list/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const textElement = document.querySelector ('.js-text');
+const listElement = document.querySelector ('.js-list');
+
+
 const tasks = [
   { name: 'Recoger setas en el campo', completed: true },
   { name: 'Comprar pilas', completed: true },
@@ -10,3 +14,53 @@ const tasks = [
   }
 ];
 
+// textElement.innerHTML += `Hay ${tasks.length} tareas`;
+
+
+function paintTasks(){
+  let html = "";
+  let className = "";
+  let checked = "";
+  for (let i = 0; i < tasks.length; i++){
+    let task = tasks[i];
+    if(task.completed === true){
+      className = "completed-task";
+      checked = "checked";
+    } else {
+      className = "";
+      checked = "";
+    }
+    html += `<li class="${className}">`;
+    html += `<input type="checkbox" class="js-checkbox" value="${i}" ${checked}/>`;
+    html += `${task.name}</li>`;
+}
+listElement.innerHTML = html;
+listenClickChecked();
+}
+paintTasks();
+
+function listenClickChecked(){
+  const checkboxElements = document.querySelectorAll (".js-checkbox");
+  for(let i = 0; i < checkboxElements.length; i++){
+    checkboxElements[i].addEventListener("change", handlerCheck);
+  }
+}
+
+function handlerCheck(event){
+  const clicked = event.target.value;
+  if(tasks[clicked].completed === true){
+    tasks[clicked].completed = false;
+  } else {
+    tasks[clicked].completed = true;
+  }
+  paintTasks();
+
+
+// // Bucle que recorra el array para extraer los true y false en completed
+//   for(let i = 0; i < clicked.length; i++){
+//     const results = `Tienes ${tasks.length} tareas. ${tasks.completed} completadas y ${tasks.completed} por realizar`;
+//     //Añado párrafo para las Tareas totales
+//     textElement.innerHTML = `<p class="${results}"></p>`;
+//   }
+  
+}
