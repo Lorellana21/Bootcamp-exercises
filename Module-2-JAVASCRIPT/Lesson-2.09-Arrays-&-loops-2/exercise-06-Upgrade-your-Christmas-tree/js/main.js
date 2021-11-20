dc9317e8486a4eb894ab04e4a80f9d532f66d0b8
Lorellana <lorellana.ordonez@gmail.com>
2021-11-20 17:21:02 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-06-Upgrade-your-Christmas-tree/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.09-Arrays-&-loops-2/exercise-06-Upgrade-your-Christmas-tree/js/main.js
@@ -1,37 +1,20 @@
 'use strict';
 
 let triangle = '▲';
+let star = "★";
+let trunk = "|";
+
 
 function paintTree(lines){
+  console.log(star);
+
   for (let i = 0; i < lines; i++){
     console.log(triangle.repeat(i+1));/*El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados.*/
   }
+  console.log(trunk);
 }
 paintTree(5);
 
 
 
-
-
-// function generatePyramid(num) {
-//   let triangle = '▲';
-
-//   for (let i = 1; i <= num; i++) {
-//     console.log(triangle += triangle);
-//   }
-// }
-// generatePyramid(5);
-
-
-/*Genera 5 arrays*/
-/*function generatePyramid(num) {
-  for (var i = 1; i <= num; i++) {
-    var arr = [];
-    for (var j = 1; j <= i; j++) {
-      arr.push("▲");
-    }
-    console.log(arr);
-  }
-}
-generatePyramid(5);*/
 
