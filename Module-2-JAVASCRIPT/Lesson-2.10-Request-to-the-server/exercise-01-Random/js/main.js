40d20229551cc6909f8faae96d51021a30914a2e
Lorellana <lorellana.ordonez@gmail.com>
2021-11-22 11:40:46 +0100

    add min/max

--- Module-2-JAVASCRIPT/Lesson-2.10-Request-to-the-server/exercise-01-Random/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.10-Request-to-the-server/exercise-01-Random/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getInteger() {
-    fetch("https://api.rand.fun/number/integer")
+    fetch("https://api.rand.fun/number/integer?min=0&max=10000")
       .then(response => response.json())
       .then(data => {
         document.querySelector('.js-result').innerHTML = data.result
