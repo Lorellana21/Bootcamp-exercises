c989118ed0bc509ebf6dadbbb39464b9e3e20324
Lorellana <lorellana.ordonez@gmail.com>
2021-11-23 09:47:20 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.10-Request-to-the-server/exercise-03/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.10-Request-to-the-server/exercise-03/js/main.js
@@ -1,13 +1,26 @@
 'use strict';
 
-function getDogImage() {
-  fetch("https://dog.ceo/api/breed/chihuahua/images/random")
-    .then(response => response.json())
-    .then(data => {
-      const img = document.querySelector("img");
-      img.src = data.message;
-      img.alt = "Un perro";
+function getInfo() {
+  //Input donde la usuaria pone su nombre de usuario
+  const username = document.querySelector(".js-input");
+  //valor del input
+  const usernameAcepted = username.value;
+
+  fetch(`https://api.github.com/users/${usernameAcepted}`)
+    .then((response) => response.json())
+    .then((data) => {
+      //li donde aparece su nombre
+      const nameElement = document.querySelector(".js-name");
+      nameElement.innerHTML = data.name;
+      //li donde aparece su numero de repo
+      const repoNumberElement = document.querySelector(".js-repoNumber");
+      repoNumberElement.innerHTML = data.public_repos;
+      //li donde aparece su avatar
+      const avatarElement = document.querySelector("img");
+      avatarElement.src = data.avatar_url;
     });
 }
-const btn = document.querySelector(".js-dog");
-btn.addEventListener("click", getDogImage);
+
+//Botón de buscar
+const buttonElement = document.querySelector(".js-btn");
+buttonElement.addEventListener("click", getInfo);
