920a0134ece1366016d8d2721add12b9ebd92576
Lorellana <lorellana.ordonez@gmail.com>
2021-12-06 12:40:59 +0100

    redone

--- Module-2-JAVASCRIPT/Lesson-2.07-Arrays-&-loops/exercise-10/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.07-Arrays-&-loops/exercise-10/js/main.js
@@ -10,14 +10,14 @@ const items = [
     }
   ];
 
-  console.log(items);
+  console.log(items);//Array de 4 elementos
+  //El tercer elemento es otro array
+  //El cuarto elemento es un objeto
 
   for (let i = 0; i < items.length; i++){
-    console.log(`el dato ${items[i]} está en la posición ${items.indexOf(items[i])} y es de tipo ${items[i].typeof}.`);
-    // console.log(`El dato ${items[i]} está en la posición ${i} y es de tipo ${items[i].typeof}.`);
+    console.log(`el dato ${items[i]} está en la posición ${items.indexOf(items[i])} y es de tipo ${items[i].typeof}`);
+    
+    // console.log(`el dato ${items[i].mother} está en la posición ${items.indexOf(items[i].mother)}`);
+    // console.log(`el dato ${items[i].father} está en la posición ${items.indexOf(items[i].father)}`);
   }
-
   
-
-
-
