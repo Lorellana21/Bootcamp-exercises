3b3c1e59d4c1ae528b3314439f183f12958d9d01
Lorellana <lorellana.ordonez@gmail.com>
2021-12-10 17:27:53 +0100

    done

--- Module-2-JAVASCRIPT/Lesson-2.13-Array-functional-methods/exercise-10-find-the-user/js/main.js
+++ Module-2-JAVASCRIPT/Lesson-2.13-Array-functional-methods/exercise-10-find-the-user/js/main.js
@@ -1,26 +1,37 @@
-`use strict`;
-
-/*a) Incidencia asociada al PIN 5232: encontrar el usuario que corresponde a ese PIN. Usa el método find.*/
+'use strict';
 
 const users = [
-  { name: "María", isPremium: false, pin: 2389 },
-  { name: "Lucía", isPremium: true, pin: 2384 },
-  { name: "Susana", isPremium: true, pin: 2837 },
-  { name: "Rocío", isPremium: false, pin: 5232 },
-  { name: "Inmaculada", isPremium: false, pin: 8998 },
+  { name: 'María', isPremium: false, pin: 2389 },
+  { name: 'Lucía', isPremium: true, pin: 2384 },
+  { name: 'Susana', isPremium: true, pin: 2837 },
+  { name: 'Rocío', isPremium: false, pin: 5232 },
+  { name: 'Inmaculada', isPremium: false, pin: 8998 }
 ];
 
-//const incidence = users.find((user) => user.pin === 5232);
+// Usuario con un pin concreto
+const userPin = users.find(user => user.pin === 5232);
+console.log(userPin.name);
 
-/*b) Usa el método findIndex para encontrar su posición y bórralo usando splice.*/
+// Borrar el usuario con pin concreto de nuestro array de usuarios
+// 1º Buscamos su ubicación de indíce dentro de nuestro array
+const indexPin5232 = users.findIndex((user) => user.pin === 5232);
+console.log(indexPin5232)
 
-const incidence = users.findIndex((user) => user.pin === 5232);
-const usersNew = users.splice(3, 1);
-//console.log(usersNew); //solo me aparece Rocio
+// 2º Extraemos ese indice de nuestro array, nuestro array principal queda modificado
+const newUsers = users.splice(3, 1);
+console.log(users);
 
+// la nueva constante seria solo los elementos borrados de nuestro array
+console.log(newUsers);
+
+//Other option
 users.splice(
-  users.findIndex((user) => user.pin === 5232),
-  1
+  users.findIndex((user) => user.pin === 5232)
 );
 
-console.log(users);
+
+
+
+
+
+
---
+++ Module-2-JAVASCRIPT/Lesson-2.13-Array-functional-methods/exercise-11-Sorting-the-race/readme.md
@@ -0,0 +1,13 @@
+## Clasificación de la carrera
+
+Volviendo a nuestra carrera de escobas, queremos tener el array del ejercicio 8 ordenado para poder tener una clasificación de la carrera: ¿nos ayudar a hacerlo usando `sort`?
+
+PISTA: la función que le pasamos a sort toma como parámetros 2 elementos del array, así que para acceder a una propiedad de un objeto en la función podemos hacerlo con el operador punto (`a.time`), como hemos hecho hasta ahora ;).
+
+---
+
+## Sorting the race
+
+Going back to our broom race, we want to have the array of exercise 8 sorted in order to have a classification of the race: can you help us to do it using `sort`?
+
+HINT: the function we pass to sort takes as parameters 2 elements of the array, so to access a property of an object in the function we can do it with the dot operator (`a.time`), as we have done so far ;).
