81636f1407b6a366c10cd9fc3afddb58897d91a8
Lorellana <lorellana.ordonez@gmail.com>
2021-12-16 17:31:57 +0100

    phase 1 done

--- Module-2-JAVASCRIPT/More-Extra-Bonus-exercises/Functions-and-parameters-from-zero/js/main.js
+++ Module-2-JAVASCRIPT/More-Extra-Bonus-exercises/Functions-and-parameters-from-zero/js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+//1: a function that writes `potato` 10 times in the console, the function will be called `ten()`
+
+function ten() {
+    for (let i = 0; i < 10; i++) {
+      console.log("patata");
+    }
+  }
+  ten();
