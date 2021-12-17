5cde226b00a0b283a874db3ac41ef33d146b31bd
Lorellana <lorellana.ordonez@gmail.com>
2021-12-17 11:20:44 +0100

    task 2 done

--- Module-2-JAVASCRIPT/More-Extra-Bonus-exercises/Functions-and-parameters-from-zero/js/main.js
+++ Module-2-JAVASCRIPT/More-Extra-Bonus-exercises/Functions-and-parameters-from-zero/js/main.js
@@ -2,9 +2,18 @@
 
 //1: a function that writes `potato` 10 times in the console, the function will be called `ten()`
 
-function ten() {
-    for (let i = 0; i < 10; i++) {
-      console.log("patata");
-    }
-  }
-  ten();
+// function ten() {
+//     for (let i = 0; i < 10; i++) {
+//       console.log("patata");
+//     }
+//   }
+//   ten();
+
+//2: modify our `ten()` function to accept a `word` parameter, and we're going to call the function 3 times, passing it a different word each time:
+
+function ten(word){
+    console.log(word);
+}
+ten("potato")
+ten("Avocado")
+ten("Pizza")
