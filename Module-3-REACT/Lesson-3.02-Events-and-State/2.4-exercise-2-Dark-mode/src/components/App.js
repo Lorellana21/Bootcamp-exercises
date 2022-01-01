c80d8d74bc7752cc5617bd06221dad99d796d6cc
Lorellana <lorellana.ordonez@gmail.com>
2022-01-01 21:06:49 +0100

    almost finished, just the last part is to be done

--- Module-3-REACT/Lesson-3.02-Events-and-State/2.4-exercise-2-Dark-mode/src/components/App.js
+++ Module-3-REACT/Lesson-3.02-Events-and-State/2.4-exercise-2-Dark-mode/src/components/App.js
@@ -1,16 +1,38 @@
-import '../styles/App.css';
+import "../styles/App.scss";
+import { useState } from "react";
 
 function App() {
-  const handleInput = (ev) => {
+  const [color, setTheme] = useState("default");
+  const [appear, setAppear] = useState("");
+
+  const handleColor = (ev) => {
     ev.preventDefault();
-    console.log('La última tecla pulsada es: ', ev.key);
-    console.log('El valor del input es: ', ev.target.value);
+    if (color === "default") {
+      setTheme("modeDark");
+      setAppear("hidden");
+    } else {
+      setTheme("default");
+      setAppear("Tienes activado el dark mode");
+    }
   };
+
   return (
-    <div>
-      <label htmlFor="name">Write down your favourite movie: </label>
-     <input type="text" id="name" name="movie" onKeyUp={handleInput} 
-     />
+    <div className={color}>
+      <button onClick={handleColor}>Des / activar el dark mode</button>
+      <p className={appear}>Tienes activado el dark mode</p>
+      <p>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+        las industrias desde el año 1500, cuando un impresor (N. del T. persona
+        que se dedica a la imprenta) desconocido usó una galería de textos y los
+        mezcló de tal manera que logró hacer un libro de textos especimen. No
+        sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno
+        en documentos electrónicos, quedando esencialmente igual al original.
+        Fue popularizado en los 60s con la creación de las hojas "Letraset", las
+        cuales contenian pasajes de Lorem Ipsum, y más recientemente con
+        software de autoedición, como por ejemplo Aldus PageMaker, el cual
+        incluye versiones de Lorem Ipsum.
+      </p>
     </div>
   );
 }
