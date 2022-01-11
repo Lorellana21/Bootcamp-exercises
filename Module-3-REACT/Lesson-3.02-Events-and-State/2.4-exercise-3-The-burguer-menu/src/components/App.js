784deafe58df5068b169d0d51acda8581436d797
Lorellana <lorellana.ordonez@gmail.com>
2022-01-11 16:04:59 +0100

    not done

--- Module-3-REACT/Lesson-3.02-Events-and-State/2.4-exercise-3-The-burguer-menu/src/components/App.js
+++ Module-3-REACT/Lesson-3.02-Events-and-State/2.4-exercise-3-The-burguer-menu/src/components/App.js
@@ -1,17 +1,9 @@
 import '../styles/App.css';
 
 function App() {
-  const handleInput = (ev) => {
-    ev.preventDefault();
-    console.log('La última tecla pulsada es: ', ev.key);
-    console.log('El valor del input es: ', ev.target.value);
-  };
+  
   return (
-    <div>
-      <label htmlFor="name">Write down your favourite movie: </label>
-     <input type="text" id="name" name="movie" onKeyUp={handleInput} 
-     />
-    </div>
+    
   );
 }
 
