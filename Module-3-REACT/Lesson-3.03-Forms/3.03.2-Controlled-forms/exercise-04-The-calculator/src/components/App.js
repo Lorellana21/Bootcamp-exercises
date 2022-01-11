ef2189abc62b68ee6eb1084afcbcf7c5e7d629fd
Lorellana <lorellana.ordonez@gmail.com>
2022-01-11 12:35:18 +0100

    add the select, working only with the sum

--- Module-3-REACT/Lesson-3.03-Forms/3.03.2-Controlled-forms/exercise-04-The-calculator/src/components/App.js
+++ Module-3-REACT/Lesson-3.03-Forms/3.03.2-Controlled-forms/exercise-04-The-calculator/src/components/App.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 const App = () => {
   const [numberA, setNumberA] = useState(0);
   const [numberB, setNumberB] = useState(0);
-
+  const [operation, setOperation] = useState("");
+  const [sum, setSum] = useState("");
+  const [rest, setRest] = useState("");
+  const [multiply, setMultiply] = useState("");
+  const [divide, setDivide] = useState("");
+  //const [total, setTotal] = useState(0);
   const handleNumberA = (ev) => {
     setNumberA(ev.target.value);
   };
@@ -12,24 +17,55 @@ const App = () => {
     setNumberB(ev.target.value);
   };
 
-  const total = parseInt(numberA) + parseInt(numberB);
+  let totalSum = parseInt(numberA) + parseInt(numberB);
+  let totalRest = parseInt(numberA) - parseInt(numberB);
+  let totalMultiply = parseInt(numberA) * parseInt(numberB);
+  let totalDivide = parseInt(numberA) / parseInt(numberB);
+
+  const handleSelectChange = (ev) => {
+    setOperation(ev.target.value);
+    //console.log(ev.target.value);
+    if (setOperation === sum) {
+      setSum(totalSum);
+    } else if (setOperation === rest) {
+      setRest(totalRest);
+    } else if (setOperation === multiply) {
+      setMultiply(totalMultiply);
+    } else {
+      setDivide(totalDivide);
+    }
+  };
 
   return (
     <div>
       <h1>La calculadora:</h1>
       <form>
+        <label htmlFor="operation">Operaciones:</label>
+
+        {/* Creamos un evento de tipo change en este select que va a ser manejada por handleSize */}
+        <select name="operation" id="operation" onChange={handleSelectChange}>
+          <option value=""></option>
+          <option value="Sumar">Sumar</option>
+          <option value="Restar">Restar</option>
+          <option value="Multiplicar">Multiplicar</option>
+          <option value="Dividir">Dividir</option>
+        </select>
+        <br></br>
+        <br></br>
         <label>
           Escribe un número:
           <input type="number" name="name" onChange={handleNumberA} />
         </label>
+        <br></br>
         <label>
           Escribe otro número:
           <input type="number" name="email" onChange={handleNumberB} />
         </label>
       </form>
       <p>
-        La suma de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
-        <strong> {total}</strong>.
+        El resultado de {operation} <strong>{numberA}</strong> y{" "}
+        <strong>{numberB}</strong> es
+        <strong> {totalSum}</strong>.
       </p>
     </div>
   );
