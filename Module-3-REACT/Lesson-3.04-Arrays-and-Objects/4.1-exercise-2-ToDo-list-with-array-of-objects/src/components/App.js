7a857d2a21a99480175c2d9bdd577f80da75ff30
Lorellana <lorellana.ordonez@gmail.com>
2022-01-12 16:51:25 +0100

    done

--- Module-3-REACT/Lesson-3.04-Arrays-and-Objects/4.1-exercise-2-ToDo-list-with-array-of-objects/src/components/App.js
+++ Module-3-REACT/Lesson-3.04-Arrays-and-Objects/4.1-exercise-2-ToDo-list-with-array-of-objects/src/components/App.js
@@ -1,17 +1,23 @@
 const App = () => {
   const tasks = [
-    "Comprar harina, jamón y pan rallado",
-    "Hacer croquetas ricas",
-    "Ir a la puerta de un gimnasio",
-    "Comerme las croquetas mirando a la gente que entra en el gimnasio",
+    { task: "Comprar harina, jamón y pan rallado", completed: true },
+    { task: "Hacer croquetas ricas", completed: true },
+    { task: "Ir a la puerta de un gimnasio", completed: false },
+    {
+      task: "Comerme las croquetas mirando a la gente que entra en el gimnasio",
+      completed: false,
+    },
   ];
 
   const title = "Mi lista de tareas";
 
   const renderTasks = () => {
     return tasks.map((task) => {
-      //console.log(task);
-      return <li>{task}</li>;
+      if (task.completed === true) {
+        return <li className="completed">{task.task}</li>;
+      } else {
+        return <li>{task.task}</li>;
+      }
     });
   };
 
