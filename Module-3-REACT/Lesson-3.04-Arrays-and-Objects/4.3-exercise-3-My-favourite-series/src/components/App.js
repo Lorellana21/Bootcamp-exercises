85705a2bcbf2feb85e9540e95ef5ad488e5b46d7
Lorellana <lorellana.ordonez@gmail.com>
2022-01-15 21:26:21 +0100

    array favorites updated but the state of the series array still change

--- Module-3-REACT/Lesson-3.04-Arrays-and-Objects/4.3-exercise-3-My-favourite-series/src/components/App.js
+++ Module-3-REACT/Lesson-3.04-Arrays-and-Objects/4.3-exercise-3-My-favourite-series/src/components/App.js
@@ -1,48 +1,57 @@
-// Fichero src/components/App.js
 import { useState } from "react";
 
 const App = () => {
-  const [series, setSeries] = useState([
+  // Estados
+
+  const [series] = useState([
     { id: "123", name: "Juego de tronos" },
     { id: "456", name: "Las chicas Gilmore" },
     { id: "678", name: "Gambita de Dama" },
   ]);
-
-  const [favorites, setFavorites] = useState([]);
-  console.log(favorites);
+  const [favorites, setFavorites] = useState([...series]);
+  const [searchName, setSearchName] = useState("");
   const [searchIsFavorite, setSearchIsFavorite] = useState(false);
 
-  // Función manejadora que se ejecuta cuando la usuaria pulsa en una serie
+  // Eventos
+
   const handleFavorite = (ev) => {
-    // Obtenemos el id de la serie pintada. Este id lo estamos renderizando dentro del map
-    // Por cierto aquí solo puedo usar currentTarget y no target porque quiero leer el id de li escuchado
     const clickedSerieId = ev.currentTarget.id;
-    // Buscamos la serie pulsada
     const foundSerie = series.find((serie) => serie.id === clickedSerieId);
-    // Invertimos la propiedad de serie pintada, si antes era false ahora es true y viceversa
     foundSerie.isFavorite = !foundSerie.isFavorite;
-    // Guardamos las series en el estado usando spread (lo explicamos más abajo)
     setFavorites([...favorites]);
+    console.log(favorites);
   };
+
+  const handleSearchName = (ev) => {
+    setSearchName(ev.target.value);
+  };
+
+  const handleSearchIsFavorite = (ev) => {
+    setSearchIsFavorite(ev.target.checked);
+  };
+
+  // Funciones de renderizado
 
   const renderSeries = () => {
     return (
       series
+        // Filtramos por nombre
+        .filter((serie) => {
+          return serie.name.toLowerCase().includes(searchName.toLowerCase());
+        })
         // Filtramos por favorito
-        .filter((favorite) => {
+        .filter((serie) => {
           if (searchIsFavorite === true) {
-            return favorite.isFavorite === true;
+            return serie.isFavorite === true;
           } else {
             return true;
           }
         })
-
+        // Mapeamos
         .map((serie) => {
           return (
-            // Renderizamos cada serie añdiendo el atributo id
             <li key={serie.id} id={serie.id} onClick={handleFavorite}>
               <h2>{serie.name}</h2>
-              {/* Pintamos si cada serie es favorita usando el atributo isFavorite */}
               <p>Es mi serie favorita: {serie.isFavorite ? "Sí" : "No"}</p>
             </li>
           );
@@ -52,8 +61,26 @@ const App = () => {
 
   return (
     <div>
-      <h1>Lista de series favoritas:</h1>
+      <h1>Lista de series:</h1>
+
       <ul>{renderSeries()}</ul>
+
+      <form>
+        <label htmlFor="searchName">Buscar por nombre de serie</label>
+        <input
+          type="text"
+          id="searchName"
+          value={searchName}
+          onChange={handleSearchName}
+        />
+        <label htmlFor="searchIsFavorite">Mostrar solo las favoritas</label>
+        <input
+          type="checkbox"
+          id="searchIsFavorite"
+          checked={searchIsFavorite}
+          onChange={handleSearchIsFavorite}
+        />
+      </form>
     </div>
   );
 };
