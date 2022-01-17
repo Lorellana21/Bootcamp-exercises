4783a216db473aefde25a390b4a35208b4c54c2b
Lorellana <lorellana.ordonez@gmail.com>
2022-01-17 17:35:42 +0100

    done

--- Module-3-REACT/Lesson-3.05-Fetch-and-local-storage/5.2-exercise-2-Series-search-engine/src/components/App.js
+++ Module-3-REACT/Lesson-3.05-Fetch-and-local-storage/5.2-exercise-2-Series-search-engine/src/components/App.js
@@ -1,1 +1,58 @@
+import { useEffect, useState } from "react";
+import callToApi from "../services/api";
 
+const App = () => {
+  // Estados
+
+  const [series, setSeries] = useState([]);
+  // Creamos searchName para gestionar el input de búsqueda, por eso es un string vacío
+  const [searchSeries, setSearchSerie] = useState("");
+
+  // useEffect
+
+  useEffect(() => {
+    callToApi(searchSeries).then((response) => {
+      setSeries(response);
+    });
+  }, [searchSeries]);
+
+  // Eventos
+
+  const handleSearchSerie = (ev) => {
+    setSearchSerie(ev.target.value);
+  };
+
+  // Renderizado
+
+  const renderSeries = () => {
+    return series.map((serie) => {
+      return (
+        <li key={serie.show.id} id={serie.show.id}>
+          Nombre: {serie.show.name}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Llamar al API de Series:</h1>
+
+      <form>
+        <label htmlFor="name">Busca por el nombre de tu serie favorita:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={searchSeries}
+          onChange={handleSearchSerie}
+        />
+      </form>
+
+      <h2>Series con el nombre: {searchSeries}</h2>
+      <ul>{renderSeries()}</ul>
+    </div>
+  );
+};
+
+export default App;
--- Module-3-REACT/Lesson-3.05-Fetch-and-local-storage/5.2-exercise-2-Series-search-engine/src/services/api.js
+++ Module-3-REACT/Lesson-3.05-Fetch-and-local-storage/5.2-exercise-2-Series-search-engine/src/services/api.js
@@ -1,1 +1,12 @@
+const callToApi = (searchSerie) => {
+  // Llamamos al API
+  return fetch(`https://api.tvmaze.com/search/shows?q=${searchSerie}`)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
 
+      return response;
+    });
+};
+
+export default callToApi;
