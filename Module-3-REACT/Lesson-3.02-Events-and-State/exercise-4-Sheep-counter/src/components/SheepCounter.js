0ee32395fe082824335ff957c970e119d15c78aa
Lorellana <lorellana.ordonez@gmail.com>
2022-01-25 17:20:01 +0100

    update

--- Module-3-REACT/Lesson-3.02-Events-and-State/exercise-4-Sheep-counter/src/components/SheepCounter.js
+++ Module-3-REACT/Lesson-3.02-Events-and-State/exercise-4-Sheep-counter/src/components/SheepCounter.js
@@ -3,6 +3,7 @@ import React from "react";
 class SheepCounter extends React.Component {
   constructor(props) {
     super(props);
+    // así inicializamos el estado
     this.state = {
       counter: 0,
     };
@@ -10,14 +11,21 @@ class SheepCounter extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(ev) {
+    // this.state siempre recibe un objeto
+    // esta es la forma buena
+    // le paso una función a React y él la ejecuta cuando el estado está actualizado y listo
     this.setState((prevState) => {
+      //es lo mismo que esto: this.setState(function (prevState) {
       return {
         counter: prevState.counter + 1,
       };
     });
+    // esta es la forma mala, se que el estado depende de sí mismo porque estoy calculando el nuevo valor de counter a partir de this.state.counter
+    // this.setState({ counter: this.state.counter + 1 });
   }
 
   render() {
+    // es bueno pintar el estado en la primera línea del render
     console.log("El estado es", this.state);
     const result = [];
     for (let index = 0; index < this.state.counter; index++) {
@@ -26,6 +34,7 @@ class SheepCounter extends React.Component {
     }
     return (
       <div className="content">
+        {/* así pintamos un campo del estado */}
         <p className="numberOfSheeps">{this.state.counter}</p>
         <button onClick={this.handleClick} className="button">
           Add a Sheep
