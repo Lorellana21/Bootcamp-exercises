396193604dffe95ada523b03dcb7e4b276be1e63
Lorellana <lorellana.ordonez@gmail.com>
2022-01-26 20:42:35 +0100

    done

---
+++ Module-3-REACT/Lesson-3.02-Events-and-State/exercises-with-Class-component/exercise-in-classroom-Events-in-one-level/README.md
@@ -0,0 +1,19 @@
+# Consolea la tecla pulsada
+
+Para familiarizarte con la sintaxis de los eventos en React te proponemos que hagas el siguiente ejercicio:
+
+1. Crea un proyecto a partir de tu React starter kit.
+1. Crea un formulario con un input de tipo texto.
+1. Crea la funcionalidad para que cuando la usuaria escriba un algo en el input, se muestre en la consola cuál ha sido la última tecla pulsada.
+1. Si pulsas intro la página se refrescará. Crea la funcionalidad para evitar que esto pase.
+
+---
+
+# Console the key pressed
+
+To familiarise yourself with the syntax of events in React, we suggest you do the following exercise:
+
+1. Create a project from your React starter kit.
+1. Create a form with a text input.
+1. Create the functionality so that when the user types something in the input, the last key pressed is shown in the console.
+1. If you press enter the page will refresh. Create the functionality to avoid this happening.
---
+++ Module-3-REACT/Lesson-3.02-Events-and-State/exercises-with-Class-component/exercise-in-classroom-Events-in-one-level/src/components/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  //Funciones manejadores de eventos
+  //Manejador para el input
+  handleInput(ev) {
+    console.log(
+      "Han cambiado el input con el value ",
+      ev.target.value,
+      " con el evento es",
+      ev
+    );
+  }
+  //Manejador para el formulario
+  handleForm(ev) {
+    ev.preventDefault();
+    console.log("Han cambiado el formulario con el evento es", ev);
+  }
+
+  //Manejador para el Button
+  handleButton(ev) {
+    console.log("Han pulsado el boton con el evento es", ev);
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Eventos en React</h1>
+        <h2>Un solo nivel de componentes</h2>
+        <h2>Y con componente de clase</h2>
+        <form onSubmit={this.handleForm}>
+          <label className="form__label" htmlFor="email">
+            Escribe tu email
+          </label>
+          <input
+            className="form__input-text display-block"
+            type="text"
+            name="email"
+            id="email"
+            onChange={this.handleInput}
+          />
+          <button className="form__btn" onClick={this.handleButton}>
+            Púlsame
+          </button>
+        </form>
+        <p>Fecha del último renderizado: {new Date().toLocaleString()}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
