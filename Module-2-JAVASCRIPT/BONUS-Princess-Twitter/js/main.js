70409a6b0c6919e0adf578285c090f619f74d0c0
Lorellana <lorellana.ordonez@gmail.com>
2022-02-05 17:15:15 +0100

    update

--- Module-2-JAVASCRIPT/BONUS-Princess-Twitter/js/main.js
+++ Module-2-JAVASCRIPT/BONUS-Princess-Twitter/js/main.js
@@ -1,30 +1,25 @@
 'use strict';
 
-const ENDPOINT = 'https://beta.adalab.es/ejercicios-extra/js-fetch-arrays-princesas-disney/data/users.json'
+const ENDPOINT = './data/users.json';
 
 let users = [];
 let favorites = [];
-const list = document.querySelector (".js-list");
+const list = document.querySelector(".js-list");
 
-/* Do your magic! 🦄🦄🦄*/
-//When loading the page we have to paint a list of princesses with the following information:
-
-/*- Name
-- Photo
-- Comment of the princess*/
 
 const getPrincess = () => {
-    fetch(ENDPOINT)
-  .then(response => response.json())
-  .then(data => {
-    
-    for (const princess of data){
-        list.innerHTML += `<li><h2>${princess.name}</h2><br><p>${princess.comment}</p> <br><img src="${princess.picture}" alt="${princess.name}"></li>`
-    }
-  })
+  fetch(ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+
+
+      for (const princess of data) {
+        list.innerHTML += `<li class="list"><h2 class="title" >${princess.name}</h2><br><p>${princess.comment}</p> <br><img class="img" src="${princess.picture}" alt="${princess.name}"></li>`
+      }
+    })
 }
 
-function paintBackground(){
+function paintBackground() {
   console.log(event.currentTarget);
   const selectedPrincess = event.target;
   selectedPrincess.classList.add("lightBlue");
